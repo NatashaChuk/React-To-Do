d603fe0b75105989ee64f86ee7570ae3d0a869ed
Natasha Hall <nhall@vision-ins.net>
2018-09-25 18:27:40 -0700

    initial commit

---
+++ src/components/ToDo.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import ToDo from './components/ToDo.js';
+
+class ToDo extends Component {
+	render() {
+		return (
+			<ul>
+				<li "Wash Clothes" />
+				<li "Cook" />
+			</ul>
+		);
+	}	
+}
+
+export default ToDo;
